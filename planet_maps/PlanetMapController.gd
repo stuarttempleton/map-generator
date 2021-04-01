36f3e31b9b4315a323ce37be12f5ff675c114428
Stuart Templeton <bvrn007@gmail.com>
2021-03-31 18:03:16 -0700

    word generation

--- planet_maps/PlanetMapController.gd
+++ planet_maps/PlanetMapController.gd
@@ -5,14 +5,16 @@ export var MapDetailBoilerPlate = "Type: %s"
 
 func _ready():
 	
-	#_generate_planet_map(StarMapData.GetRandomPlanetByType("Comet"), 100)
-	_generate_planet_map(-1, "Test Map", "Desert")
+	_generate_planet_map(-1, "", "")
 	pass
 
 
 
 func _generate_planet_map(_seed, _name, _type):
 	
+	if (_name == ""): _name = WordGenerator.CreateWord()
+	if (_type == ""): _type = $PlanetMap.PlanetGradientMaps.keys()[randi() % $PlanetMap.PlanetGradientMaps.keys().size() - 1]
+		
 	$PlanetMap.visible = true
 	$PlanetName.text = _name
 	$PlanetInformation.text = MapDetailBoilerPlate % [_type]
@@ -20,4 +22,4 @@ func _generate_planet_map(_seed, _name, _type):
 
 
 func _on_generate_button_pressed(planet_type):
-	$PlanetSurfaceMap/PlanetMap._generate(-1, "Test Map", planet_type)
+	$PlanetSurfaceMap/PlanetMap._generate(-1, "", planet_type)
